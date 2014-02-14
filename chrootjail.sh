947353034cd21956a2949a90b24d30a3f10cb0b6
alexriz <alexriz.webdev@gmail.com>
2014-02-14 14:14:43 +0200

    rename script

--- chrootjail.sh
+++ chrootjail.sh
@@ -3,3 +3,11 @@
 # (c) Copyright by Alex Yegerev (alexriz)
 #
 #####################################################################
+
+# Print help guide
+if [[ "$1" == "-h" || "$1" == "--help" ]] ; then
+	echo "Help!"
+fi
+echo "test"
+
+exit
